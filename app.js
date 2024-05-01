0d215504d78bef2625d4b84d30a53cc0d36c5205
diasoy <22082010134@student.upnjatim.ac.id>
2024-05-01 20:30:50 +0700

    membuat model place & contoh data

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost:27017/bestpoints")
+  .then((result) => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const Place = require("./models/place");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/seed/place", async (req, res) => {
+  const place = new Place({
+    title: "Empire State Building",
+    price: "$99999",
+    description: "This is the best place in the world",
+    location: "New York, NY",
+  });
+
+  await place.save();
+  res.send(place);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
