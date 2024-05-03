fd329f4ed5f6e09df349eb30ac279be43c9b98bb
diasoy <22082010134@student.upnjatim.ac.id>
2024-05-04 01:54:12 +0700

    layouts with ejsmate

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const ejsMate = require("ejs-mate");
 const express = require("express");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
@@ -14,10 +15,12 @@ mongoose
   });
 
 const Place = require("./models/place");
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
 
 app.get("/", (req, res) => {
   res.render("home");
